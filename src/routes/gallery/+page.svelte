2784ca555dc0ab3ebe2f9f0b4e5a0d776508370d
Noah Jonas <noahjonas@MacBook-Air-von-Noah.local>
2022-12-02 11:16:10 +0100

    galery

---
+++ src/routes/gallery/+page.svelte
@@ -0,0 +1,17 @@
+<div class="main">
+	<h1>Aktuell in Arbeit! Bald verfügbar...</h1>
+</div>
+<div class="img">
+	<img src="https://societadanteberlin.files.wordpress.com/2015/02/in-bearbeitung.png" alt="main" width="700px">
+</div>
+<style>
+	.main {
+		width: auto;
+		height: 50px;
+	}
+	.img {
+		width: 500px;
+		height: 500px;
+		
+	}
+</style>
