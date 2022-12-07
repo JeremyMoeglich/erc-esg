7733c2cde7b5799467fd666b5a08e5feeb9cfb07
Noah Jonas <noahjonas@MacBook-Air-von-Noah.local>
2022-12-07 14:36:26 +0100

    changes

--- src/lib/components/site/fields/section_fields.svelte
+++ src/lib/components/site/fields/section_fields.svelte
@@ -5,11 +5,35 @@
 <Fields
 	title={'Unsere Abteilungen'}
 	fields={[
-		{ id: 'beratung', preview: 'Die Mitarbeiter der Beratung kümmern sich um das operative Geschäft und werden dafür gezielt weitergebildet.' },
-		{ id: 'kundenzufriedenheit', preview: 'Die Mitarbeiter der Kundenzufriedenheit kümmern sich um die Optimierung der Prozesse um die Zufriedenhet der Kunden zu Steigern.' },
-		{ id: 'marketing', preview: 'Die Marketingabteilung betreibt unsere Social Media Accounts und kümmert sich um die Kundenaquirirung. ' },
-		{ id: 'buchhaltung', preview: 'Die Buchhaltung kümmert sich um die Finanzen der Schülerfirma und bereitet die Generalversammlung vor.' },
-		{ id: 'it', preview: 'Die Mitareiter der IT kümmert sich um die digitale Infrastruktur und unsere Website.' },
-		{ id: 'wettbewerbe', preview: 'Als Schülerfrima sind Wettbewerbe ein Stetiger Begleiter. Deshalb kümmert sich die Wettbewerbsabteilung um die Bewerbung und Abwicklung von Wettbewerben' }
+		{
+			id: 'beratung',
+			preview:
+				'Die Mitarbeiter der Beratung kümmern sich um das operative Geschäft und werden dafür gezielt weitergebildet.'
+		},
+		{
+			id: 'kundenzufriedenheit',
+			preview:
+				'Die Mitarbeiter der Kundenzufriedenheit kümmern sich um die Optimierung der Prozesse um die Zufriedenhet der Kunden zu Steigern.'
+		},
+		{
+			id: 'marketing',
+			preview:
+				'Die Marketingabteilung betreibt unsere Social Media Accounts und kümmert sich um die Kundenaquirirung. '
+		},
+		{
+			id: 'buchhaltung',
+			preview:
+				'Die Buchhaltung kümmert sich um die Finanzen der Schülerfirma und bereitet die Generalversammlung vor.'
+		},
+		{
+			id: 'it',
+			preview:
+				'Die Mitareiter der IT kümmert sich um die digitale Infrastruktur und unsere Website.'
+		},
+		{
+			id: 'wettbewerbe',
+			preview:
+				'Als Schülerfrima sind Wettbewerbe ein Stetiger Begleiter. Deshalb kümmert sich die Wettbewerbsabteilung um die Bewerbung und Abwicklung von Wettbewerben'
+		}
 	]}
 />
