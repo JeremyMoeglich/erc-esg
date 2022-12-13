5b192180d8f195cfae5da671caf002841384b0c5
jeremy <jeremy@moeglich.dev>
2022-12-13 14:35:10 +0100

    changes

--- src/routes/mitglieder/+page.svelte
+++ src/routes/mitglieder/+page.svelte
@@ -1,74 +1,52 @@
 <script lang="ts">
-    	import { browser } from '$app/environment';
-	import Button from '$lib/components/elements/button.svelte';
-	import DbImage from '$lib/components/elements/db_image/db_image.svelte';
-	import { admin_mode } from '$lib/scripts/frontend/auth/auth_state';
-	import { get_gallery_images } from '$lib/scripts/frontend/fetch/get_gallery_images';
-	import { onMount } from 'svelte';
-	import { v4 } from 'uuid';
-	import { string } from 'zod';
-    let images: string[] | undefined = undefined;
+	import Mitglied from './mitglied.svelte';
 
-    onMount(async () => {
-		if (browser) {
-			images = await get_gallery_images();
-		}
-	});
+	interface Mitglied {
+		vorname: string;
+		nachname: string;
+		alter: number;
+		beitrittsdatum: Date;
+		abteilung: string;
+	}
 
-    export let name: string;
+	const mitglieder: Array<Mitglied> = [
+		{
+			nachname: 'Moeglich',
+			vorname: 'Jeremy',
+			beitrittsdatum: new Date("December 1, 2021"),
+			abteilung: 'IT',
+			alter: 18
+		},
+		{
+			nachname: 'Moeglich',
+			vorname: 'Jeremy',
+			beitrittsdatum: new Date("December 1, 2021"),
+			abteilung: 'IT',
+			alter: 18
+		},
+		{
+			nachname: 'Moeglich',
+			vorname: 'Jeremy',
+			beitrittsdatum: new Date("December 1, 2021"),
+			abteilung: 'IT',
+			alter: 18
+		},
+		{
+			nachname: 'Moeglich',
+			vorname: 'Jeremy',
+			beitrittsdatum: new Date("December 1, 2021"),
+			abteilung: 'IT',
+			alter: 18
+		},
+	];
 </script>
 
-
-
-<div class="main">
-
-        {#if images}
-		{#each images as image_id}
-			<div class="image">
-				<DbImage id={image_id} width={'300px'} gallery={true} attr={`w-400`} />
-                
-			</div>
-            <div class="vorname">
-                Vorname: {name}
-            </div>
-            
-            <div>
-                Nachname: {name}
-            </div>
-            <div>
-                Alter: {name}
-            </div>
-            <div>
-                Beitrittsdatum: {name}
-            </div>
-            <div>
-                Abteilung: {name}
-            </div>
-		{/each}
-		{#if $admin_mode}
-			<div class="add">
-				<Button
-					onclick={async () => {
-						images = [...(images ?? []), v4()];
-					}}
-					text={'Add Person'}
-				/>
-			</div>
-		{/if}
-	{:else}
-		<div>Loading...</div>
-	{/if}
-</div>
-
-<style>
-    .main {
-		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(min(250px, 100%), 1fr));
-		gap: 10px;
-		padding: 30px 30px;
-		width: 100%;
-		align-items: center;
-		justify-items: center;
-	}
-   
-</style>
+{#each mitglieder as mitglied}
+	<Mitglied
+		vorname={mitglied.vorname}
+		abteilung={mitglied.abteilung}
+		alter={mitglied.alter}
+		beitrittsdatum={mitglied.beitrittsdatum}
+		nachname={mitglied.nachname}
+	/>
+{/each}
---
+++ src/routes/mitglieder/mitglied.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import DbImage from '$lib/components/elements/db_image/db_image.svelte';
+
+	export let vorname: string;
+	export let nachname: string;
+	export let alter: number;
+	export let beitrittsdatum: Date;
+	export let abteilung: string;
+</script>
+
+<div class="outer">
+	<DbImage id={`mitglied-${vorname}-${nachname}`} width={"200px"}/>
+	<div>
+		Vorname: {vorname}
+	</div>
+	<div>
+		Nachname: {nachname}
+	</div>
+	<div>
+		Alter: {alter}
+	</div>
+	<div>
+		Beitrittsdatum: {beitrittsdatum.toLocaleDateString('de')}
+	</div>
+	<div>
+		Abteilung: {abteilung}
+	</div>
+</div>
+
+<style>
+    .outer {
+        display: flex;
+        gap: 20px;
+        align-items: center;
+        font-size: 20px;
+    }
+</style>
