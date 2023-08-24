8786499d4968317e90148c4c8f3326f5a0df7e6b
Noah Jonas <noahjonas@MacBook-Air-von-Noah.local>
2023-08-24 08:59:24 +0200

    spenden

---
+++ src/routes/spenden/+page.svelte
@@ -0,0 +1,20 @@
+<div class="flex">
+    <div>
+        <h2>
+            Paypal
+        </h2>
+        <a href="https://google.com">Paypal</a>
+    </div>
+    <div>
+        <h2>
+            Paypal
+        </h2>
+        <a href="https://google.com">Paypal</a>
+    </div>
+    <div>
+        <h2>
+            Paypal
+        </h2>
+        <a href="https://google.com">Paypal</a>
+    </div>
+</div>
